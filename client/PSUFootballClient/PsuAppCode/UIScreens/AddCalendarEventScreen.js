3912d83cd0ceb2559b64b53c017b0fd40fea327e
John Cantos <jrcst39@hotmail.com>
2018-04-16 13:00:16 -0400

    Made adjustments to the following files: AddCalendarEventScreen.js; GameScheduleTableViewScreen.js. Adjustments entail UI adjustment to the gameschedule screen and disabling of addcalendar method.  Parameters needed to addcalendar are passed however are not used.

--- client/PSUFootballClient/PsuAppCode/UIScreens/AddCalendarEventScreen.js
+++ client/PSUFootballClient/PsuAppCode/UIScreens/AddCalendarEventScreen.js
@@ -20,6 +20,7 @@ export default class AddCalendarEventScreen extends AbstractNavigableScreen {
             buttonTitle: 'Add Event',
             buttonDisabled: false,
             calendarAuth: 'authorized',
+            // calendarAuth: '',
             eventStartDateString: '',   // Format:  2018-05-06T18:00:00.000Z
             duration:  3,               // Duration = 3 hrs (default it!)
             location: '',
@@ -43,15 +44,14 @@ export default class AddCalendarEventScreen extends AbstractNavigableScreen {
         //  Check if we are authorized to add
         //  an event to our Calendar App
         if (this.state.calendarAuth !== 'authorized') {
-
             Alert.alert('Not authorized to add event to Calendar');
-
         } else {
 
             //  Determine the end time from the start time
             //  (we wont assume rollover of time/day.  Since
             //  events wont start that late.  (we could make this
             //  better, but it will do for our purposes
+
             let eventStartString = this.state.eventStartDateString;
             let startTimeHour = eventStartString.substr(11,2);
             let endTimeHourInt = parseInt(startTimeHour) + this.state.duration;
@@ -61,15 +61,30 @@ export default class AddCalendarEventScreen extends AbstractNavigableScreen {
             let endDateString = beginning + endTimeHour + ending;
 
 
+            console.debug('Event Start: ' + eventStartString);
+            console.debug('Event End: ' + endDateString);
+            console.debug('Location: ' + this.state.location);
+            console.debug('Description: ' + this.state.description);
+            console.debug('Notes: ' + this.state.notes);
+
+            // let startDatetime = new Date(eventStartString);
+            // let endDatetime = new Date(endDateString);
+
+            // console.debug('Event Start: ' + startDatetime.toUTCString());
+            // console.debug('Event End: ' + endDatetime.toUTCString());
 
             //  Add the respective event to our device's
             //  Calendar App
             //      reference:   https://github.com/wmcmahan/react-native-calendar-events/blob/master/README.md#saveevent
-            RNCalendarEvents.saveEvent(this.state.description, { startDate: eventStartString,
-                                                                 endDate:   endDateString,
-                                                                 location: this.state.location,
-                                                                 description: this.state.description,
-                                                                 notes: this.state.notes });
+            // RNCalendarEvents.saveEvent(this.state.description, { startDate: eventStartString,
+            //                                                      endDate:   endDateString,
+            //                                                      location: this.state.location,
+            //                                                      description: this.state.description,
+            //                                                      notes: this.state.notes });
+
+
+            // RNCalendarEvents.saveEvent('title test', { startDate: eventStartString.replace(/-/g,'').replace(/:/g,'').replace('.000',''),
+            //                                            endDate: endDateString.replace(/-/g,'').replace(/:/g,'').replace('.000','') });
 
             // Set the Component's Button state to 'Done'
             this.setState({
@@ -86,55 +101,50 @@ export default class AddCalendarEventScreen extends AbstractNavigableScreen {
 //    }
 
 
-    componentWillMount() {
+componentWillMount() {
 
 
-        // Set the Component's state
-        this.setState({
-            eventStartDateString: this.props.navigation.state.params.startDateString,
-            location: this.props.navigation.state.params.location,
-            description: this.props.navigation.state.params.description,
-            notes: this.props.navigation.state.params.notes,
-        });
+  // Set the Component's state
+  this.setState({
+    eventStartDateString: this.props.navigation.state.params.startDateString,
+    location: this.props.navigation.state.params.location,
+    description: this.props.navigation.state.params.description,
+    notes: this.props.navigation.state.params.notes
+  });
 
 
-/*   this works for android simulator, but giving me
-     problems on my OS phone
+  // this works for android simulator, but giving me
+  //   problems on my OS phone
 
 
-        // Let's get access before doing anything
-        RNCalendarEvents.authorizationStatus()
-            .then(status => {
+  // Let's get access before doing anything
+  // console.debug(RNCalendarEvents.authorizeEventStore())
+  // .then(status => {
+  //
+  //   // if the status was previous accepted, set the authorized status to state
+  //   this.setState({
+  //     calendarAuth: status
+  //   });
+  //
+  //   if (status === 'undetermined') {
+  //
+  //     // if we made it this far, we need to ask the user for access
+  //     RNCalendarEvents.authorizeEventStore()
+  //     .then((out) => {
+  //
+  //       if (out === 'authorized') {
+  //
+  //         // set the new status to the auth state
+  //         this.setState({
+  //           calendarAuth: out
+  //         });
+  //       }
+  //     })   // end then
+  //   }  // end if status
+  // }) // end then
+  // .catch(error => console.warn('Auth Error: ', error));
 
-                // if the status was previous accepted, set the authorized status to state
-                this.setState({
-                    calendarAuth: status
-                });
-
-                if (status === 'undetermined') {
-
-                    // if we made it this far, we need to ask the user for access
-                    RNCalendarEvents.authorizeEventStore()
-                        .then((out) => {
-
-                            if (out === 'authorized') {
-
-                                // set the new status to the auth state
-                                this.setState({
-                                    calendarAuth: out
-                                });
-                            }
-                        })   // end then
-                }  // end if status
-            }) // end then
-
-            .catch(error => console.warn('Auth Error: ', error));
-
-
-             */
-
-
-    }   // end componentWillMount()
+}   // end componentWillMount()
 
 
     render() {
@@ -156,7 +166,7 @@ export default class AddCalendarEventScreen extends AbstractNavigableScreen {
                         <Text style={styles.eventdetails}>Location:  {this.state.location}</Text>
                         <Text style={styles.eventdetails}>Start:  {this.state.eventStartDateString}</Text>
                         <Text style={styles.eventdetails}>Duration:   {this.state.duration} hrs</Text>
-                        <Text style={styles.eventdetails}>{this.state.notes}</Text>
+                        {/* <Text style={styles.eventdetails}>{this.state.notes}</Text> */}
                     </View>
                     <View style={styles.button}>
                         <Button title={this.state.buttonTitle}
